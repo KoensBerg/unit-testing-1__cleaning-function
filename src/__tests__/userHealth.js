6d331be01da57ee9941b210e0faef2e9bcdd0cee
Oleg Yanke <yanke.kd@gmail.com>
2023-11-19 13:11:46 +0200

    corr userHealth & .eslintrc

--- src/__tests__/userHealth.js
+++ src/__tests__/userHealth.js
@@ -1,8 +1,9 @@
 import userHealth from '../user-health';
 
 test.each([
-  [{ name: '...', health: 90 }, 'healthy'],
-  [{ name: '...', health: 45 }, 'wounded'],
+  [{ name: '...', health: 51 }, 'healthy'],
+  [{ name: '...', health: 50 }, 'wounded'],
+  [{ name: '...', health: 15 }, 'wounded'],
   [{ name: '...', health: 14 }, 'critical'],
 ])(
   'should return helth level for some user with his health',
